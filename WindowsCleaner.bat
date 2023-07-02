aea5c042f6071440107d8614b46fc8c811124143
ozpingux <99804991+ozpingux@users.noreply.github.com>
2023-07-01 21:22:05 -0500

    Update WindowsCleaner.bat

--- WindowsCleaner.bat
+++ WindowsCleaner.bat
@@ -28,7 +28,9 @@ attrib /d /s -r -h -s %userprofile%\AppData\Local\Temp\*.*
 attrib /d /s -r -h -s C:\Windows\Temp\*.*
 attrib /d /s -r -h -s C:\Windows\AppCompat\Programs\Amcache\sysmain.sdb
 attrib /d /s -r -h -s C:\Windows\AppCompat\Programs\Amcache\*.*
+attrib /d /s -r -h -s C:\Windows\appcompat\Programs\*.*
 attrib /d /s -r -h -s C:\ProgramData\Microsoft\Diagnosis\EventTranscript\*.*
+attrib /d /s -r -h -s %UserProfile%\AppData\Local\Microsoft\Windows\Notifications\*.*
 net stop WSearch
 powershell -Command "Stop-Service -Name WSearch -Force"
 attrib /d /s -r -h -s C:\ProgramData\Microsoft\Search\Data\Applications\Windows\*.*
@@ -61,6 +63,7 @@ del /f /s /q C:\ProgramData\Microsoft\Search\Data\Applications\Windows\*.*
 del /f /s /q C:\Windows\AppCompat\Programs\Amcache\sysmain.sdb
 del /f /s /q C:\Windows\AppCompat\Programs\Amcache\*.*
 del /f /s /q C:\ProgramData\Microsoft\Diagnosis\EventTranscript\*.*
+del /f /s /q C:\Windows\appcompat\Programs\*.*
 del /f /s /q C:\Windows\Prefetch\*.pf
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
 erase "%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*.*" /f /s /q
@@ -106,6 +109,7 @@ DEL /f /q %systemroot%\Prefetch\*.7db
 DEL /f /q %systemroot%\Prefetch\*.ebd
 DEL /f /q %systemroot%\Prefetch\*.bin
 DEL /f /q %systemroot%\Prefetch\*.db
+del /s /f /q C:\Windows\Prefetch\Ag*.db
 DEL /f /q %systemroot%\Prefetch\ReadyBoot\*.fx
 DEL /f /q %systemroot%\Minidump\*.*
 del /f /s /q c:\windows\logs\cbs\*.log
@@ -119,6 +123,7 @@ del /f /s /q C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\WebCache\*.log 
 del /f /s /q C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\SettingSync\*.log /s /q
 del /f /s /q C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Explorer\ThumbCacheToDelete\*.tmp /s /q
 del /f /s /q C:\Users\%USERNAME%\AppData\Local\Microsoft\"Terminal Server Client"\Cache\*.bin /s /q
+del /f /s /q %UserProfile%\AppData\Local\Microsoft\Windows\Notifications\*.*
 rmdir /q /s C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\INetCache\
 del /s /f /q %WinDir%\Temp\*.*
 del /s /f /q %Temp%\*.*
